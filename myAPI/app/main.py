0930894b9929307140a64de87e17199b84700082
YaelAlejandroUrbanoZuniga <124051558@upq.edu.mx>
2026-02-13 20:36:56 -0600

    PUT commit

--- myAPI/app/main.py
+++ myAPI/app/main.py
@@ -1,5 +1,5 @@
 # -------------------------------------IMPORTACIONES---------------------------------------
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 import asyncio
 from typing import Optional
 
@@ -30,11 +30,11 @@ async def hola():
         "estatus": "200"}
 
 
-@app.get("/v1/usuario/{id}", tags=['Parametro Obligatorio'])
+@app.get("/v1/parametroOB/{id}", tags=['Parametro Obligatorio'])
 async def consultaUno(id:int):
     return{"Se encontró usuario": id}
 
-@app.get("/v1/usuarios/", tags=['Parametro Opcional'])
+@app.get("/v1/parametroOP/", tags=['Parametro Opcional'])
 async def consultaTodos(id:Optional[int]=None):
     if id is not None:
         for usuario in usuarios:
@@ -43,4 +43,46 @@ async def consultaTodos(id:Optional[int]=None):
         return{"mensaje":"usuario no encontrado","usuario": id} 
     else:
         return{"mensaje":"NO SE PROPORCIONO ID"}     
-        
+    
+@app.get("/v1/usuarios/", tags=['CRUD HTTP'])
+async def leer_usuarios():
+    return{
+        "status":"200",
+        "total":len(usuarios),
+        "usuarios":usuarios
+    }
+
+@app.post("/v1/usuarios/", tags=['CRUD HTTP'],status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == usuario.get("id"):
+            raise HTTPException(
+                status_code=400,
+                detail="El ID ya existe"
+            )
+    usuarios.append(usuario)
+    return{
+        "mensaje":"Usuario Agregado",
+        "Usuario":usuario
+    }
+
+
+
+
+
+
+
+
+@app.put("/v1/usuarios/{id}", tags=['CRUD HTTP'], status_code=status.HTTP_204_NO_CONTENT)
+async def actualizar_usuario(id:int, usuario:dict):
+    for usr in usuarios:
+        if usr["id"] == id:
+            usr.update(usuario)
+            return{
+                "mensaje":"Usuario Actualizado",
+                "Usuario":usuario
+            }
+    raise HTTPException(
+        status_code=400,
+        detail="El ID no existe"
+    )
