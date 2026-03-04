f874f7906dfb664a2a614f230074cd14afb44785
YaelAlejandroUrbanoZuniga <124051558@upq.edu.mx>
2026-03-03 20:13:11 -0600

    Práctica 6: Protección de Endpoints

--- myAPI/app/main.py
+++ myAPI/app/main.py
@@ -1,12 +1,13 @@
 # -------------------------------------IMPORTACIONES---------------------------------------
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Depends
 
 import asyncio
 
 from typing import Optional
 
 from pydantic import BaseModel, Field
-
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
 
 # ---------------------------------INSTANCIA DEL SERVIDOR----------------------------------
@@ -32,6 +33,21 @@ class usuario_create(BaseModel):
 
 class usuario_delete(BaseModel):
     id: int = Field(..., gt = 0, description = "Identificador de usuario" )
+
+
+
+# -----------------------------------SEGURIDAD HTTP BASIC-----------------------------------
+security = HTTPBasic()
+def verificar_Peticion(credenciales:HTTPBasicCredentials = Depends(security)):
+    userAuth = secrets.compare_digest(credenciales.username,"ivanisay")
+    passAuth = secrets.compare_digest(credenciales.password,"123456")
+
+    if not(userAuth and passAuth):
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Credenciales no Autorizadas"
+        )
+    return credenciales.username
 
 
 
@@ -116,12 +132,12 @@ async def actualizar_usuario(id:int, usuario:dict):
 # ----------------------------------------DELETE----------------------------------------
 
 @app.delete("/v1/usuarios/{id}", tags=['CRUD HTTP'], status_code=status.HTTP_200_OK)
-async def eliminar_usuario(id:int):
+async def eliminar_usuario(id:int, userAuth:str = Depends(verificar_Peticion)):
     for usr in usuarios:
         if usr["id"] == id:
             usuarios.remove(usr)
             return{
-                "mensaje":"Usuario Borrado",
+                "mensaje":f"Usuario Borrado Por: {userAuth}",
                 "Usuario":usr
             }
     raise HTTPException(
